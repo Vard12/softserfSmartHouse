f5c5209b6eb20bda709e4c83301d27dcc84f1e10
Vadim <nahornyivadim@gmail.com>
2018-03-09 14:37:33 +0200

    add fridge

--- js/smartHouseContoller.js
+++ js/smartHouseContoller.js
@@ -43,20 +43,19 @@ BtnView.prototype.render = function () {
     let btnWrap = document.body.querySelector(".btnWrap"),
 
         btn = document.createElement("a"),
-        btnFridje = btn.cloneNode(true),
+        btnFridge = btn.cloneNode(true),
         btnComputer = btn.cloneNode(true);
 
     btn.className = "btn";
-    btnFridje.classList.add("btn", "fridje");
+    btnFridge.classList.add("btn", "fr");
     btnComputer.classList.add("btn", "comp");
 
     btn.innerText = "add address";
-    btnFridje.innerText = "add fridje";
+    btnFridge.innerText = "add fridge";
     btnComputer.innerText = "add computer";
 
     btnWrap.appendChild(btn);
-    btnWrap.appendChild(btnFridje);
-    btnWrap.appendChild(btnComputer);
+    btnWrap.appendChild(btnFridge);
     btnWrap.appendChild(btnComputer);
 
 
@@ -71,7 +70,7 @@ function HouseAddressView(smartHouse) {
     this.clickHandler = function () {
         let addAddressBtn = document.body.querySelector(".btn");
         addAddressBtn.addEventListener("click", () => {
-            this._smartHouse.setAddress(prompt("Enter home address","Anfield Rd, Liverpool L4 0TH, Великобритания"));
+            this._smartHouse.setAddress(prompt("Enter home address", "Anfield Rd, Liverpool L4 0TH, Великобритания"));
             this.render();
         })
     }
@@ -95,71 +94,82 @@ HouseAddressView.prototype.render = function () {
     }
 };
 
-// function FridgeView(smartHouse) {
-//     this._smartHouse = smartHouse;
-// }
-//
-// FridgeView.prototype.addFridges = function () {
-//     let smartHouse = document.body.querySelector(".bottomContainer"),
-//         fridgeCollection = this._smartHouse.getFridge(),
-//         name = null,
-//         type = null,
-//         model = null,
-//         bgColor = null,
-//         state = null;
-//     for (let i = 0; i < fridgeCollection.length; i++) {
-//         name = fridgeCollection[i].getName();
-//         type = fridgeCollection[i].getType();
-//         model = fridgeCollection[i].getModel();
-//         bgColor = fridgeCollection[i].getBgColor();
-//         state = fridgeCollection[i].getState();
-//         let fridge = document.createElement("div"),
-//             stateBtn = document.createElement("a");
-//         fridge.classList = "fridge";
-//         stateBtn.classList = "stateBtn";
-//         stateBtn.setAttribute("href", "#");
-//         fridge.innerHTML = name + "<br>" + type + "<br>" + model;
-//         if (state === false) {
-//             stateBtn.classList.add("off");
-//         } else {
-//             stateBtn.classList.add("on");
-//         }
-//         fridge.appendChild(stateBtn);
-//         fridge.style.backgroundColor = bgColor;
-//         smartHouse.appendChild(fridge);
-//     }
-// };
-// FridgeView.prototype.render = function () {
-//     let self = this;
-//     let fridgeBtn = document.body.querySelector(".btn.fridje"),
-//         fridge = document.createElement("div");
-//
-//     fridge.classList = "fridge";
-//
-//     fridgeBtn.addEventListener("click", () => {
-//         this.addFridges();
-//         this.render();
-//     });
-//
-//     if (document.body.querySelectorAll(".fridge .stateBtn").length !== 0) {
-//         let stateBtn = document.body.querySelectorAll(".fridge .stateBtn");
-//         for (let i = 0; i < stateBtn.length; i++) {
-//             stateBtn[i].addEventListener('click', function () {
-//                 let state = self._smartHouse.getFridge()[i].getState();
-//                 if (state) {
-//                     self._smartHouse.getFridge()[i].setState();
-//                     stateBtn.classList.add("off");
-//                     this.innerText = "off";
-//                 } else {
-//                     self._smartHouse.getFridge()[i].setState();
-//                     stateBtn.classList.add("on");
-//                     this.innerText = "on";
-//                 }
-//             });
-//         }
-//     }
-// };
-//
+function FridgeView(smartHouse) {
+    this._smartHouse = smartHouse;
+    this.clickHandler = function () {
+        let fridgeBtn = document.body.querySelector(".btn.fr");
+        fridgeBtn.addEventListener("click", () => {
+            this._smartHouse.setFridge(
+                new Fridge(
+                    prompt("введите марку холодильника", "Донбасс"),
+                    prompt("введите модель холодильника", "неламающийся"),
+                    prompt("введите цвет холодильника", "#4caf50"),
+                    prompt("тип холодильника", "однокамерный")));
+            this.render();
+        });
+    };
+    this.stateClickHandler = function () {
+        function indexElement(child) {
+            let i = 0;
+            while ((child = child.previousSibling) != null)
+                i++;
+            return i;
+        };
+        let bottomContainerComp = document.querySelector(".bottomContainer__fridge");
+        bottomContainerComp.addEventListener("click", (e) => {
+            if (e.target.classList.contains("stateBtn")) {
+                let indexElem = e.target.parentElement;
+                let index = indexElement(indexElem);
+                let state = this._smartHouse.getFridge()[index].getState();
+                if (state) {
+                    this._smartHouse.getFridge()[index].setState();
+                    e.target.classList.toggle("on");
+                    e.target.innerText = "off";
+                } else {
+                    this._smartHouse.getFridge()[index].setState();
+                    e.target.classList.toggle("on");
+                    e.target.innerText = "on";
+                }
+            }
+        });
+    };
+}
+
+FridgeView.prototype.render = function () {
+
+    let fridge = document.createElement("div");
+    fridge.classList = "fridge";
+
+    let bottomContainerFridge = document.body.querySelector(".bottomContainer__fridge"),
+        fridgeCollection = this._smartHouse.getFridge(),
+        name = null,
+        type = null,
+        model = null,
+        bgColor = null,
+        state = null;
+    for (let i = 0; i < fridgeCollection.length; i++) {
+        name = fridgeCollection[i].getName();
+        type = fridgeCollection[i].getType();
+        model = fridgeCollection[i].getModel();
+        bgColor = fridgeCollection[i].getBgColor();
+        state = fridgeCollection[i].getState();
+        let stateBtn = document.createElement("a");
+        stateBtn.classList = "stateBtn";
+        stateBtn.setAttribute("href", "#");
+        fridge.innerHTML = name + "<br>" + type + "<br>" + model;
+        if (state === false) {
+            stateBtn.classList.remove("off");
+            stateBtn.innerText = "off";
+        } else {
+            stateBtn.classList.add("on");
+            stateBtn.innerText = "on"
+        }
+        fridge.appendChild(stateBtn);
+        fridge.style.backgroundColor = bgColor;
+        bottomContainerFridge.appendChild(fridge);
+    }
+};
+
 function ComputerView(smartHouse) {
     this._smartHouse = smartHouse;
     this.clickHandler = function () {
@@ -169,7 +179,7 @@ function ComputerView(smartHouse) {
                 new Computer(
                     prompt("введите имя компьютера", "Apple"),
                     prompt("введите модель компьютера", "macBook pro"),
-                    prompt("введите цвет компьютера", "#eee"),
+                    prompt("введите цвет компьютера", "#4caf50"),
                     prompt("размер ram", "8гб")));
             this.render();
         });
@@ -184,28 +194,27 @@ function ComputerView(smartHouse) {
 
         let bottomContainerComp = document.querySelector(".bottomContainer__comp");
 
-        bottomContainerComp.addEventListener("click",  (e) => {
+        bottomContainerComp.addEventListener("click", (e) => {
             if (e.target.classList.contains("stateBtn")) {
                 let indexElem = e.target.parentElement;
-                let q = indexElement(indexElem);
-                let state = this._smartHouse.getComputer()[q].getState();
+                let index = indexElement(indexElem);
+                let state = this._smartHouse.getComputer()[index].getState();
                 if (state) {
-                    this._smartHouse.getComputer()[q].setState();
+                    this._smartHouse.getComputer()[index].setState();
                     e.target.classList.toggle("on");
                     e.target.innerText = "off";
                 } else {
-                    this._smartHouse.getComputer()[q].setState();
+                    this._smartHouse.getComputer()[index].setState();
                     e.target.classList.toggle("on");
                     e.target.innerText = "on";
                 }
             }
         });
-    }
+    };
 }
 
 ComputerView.prototype.render = function () {
-    let self = this,
-        computer = document.createElement("div"),
+    let computer = document.createElement("div"),
         computerCon = document.body.querySelector(".bottomContainer__comp"),
         computerCollection = this._smartHouse.getComputer(),
         name = null,
@@ -240,6 +249,8 @@ ComputerView.prototype.render = function () {
         computerCon.appendChild(computer);
     }
 };
+
+
 // create objects
 let smartHouse = new SmartHouse();
 
@@ -253,8 +264,10 @@ let houseAddressView = new HouseAddressView(smartHouse);
 houseAddressView.render();
 houseAddressView.clickHandler();
 
-// let fridgeCtrl = new FridgeView(smartHouse);
-// fridgeCtrl.render();
+let fridgeCtrl = new FridgeView(smartHouse);
+fridgeCtrl.clickHandler();
+fridgeCtrl.stateClickHandler();
+fridgeCtrl.render();
 
 let computerView = new ComputerView(smartHouse);
 computerView.clickHandler();
